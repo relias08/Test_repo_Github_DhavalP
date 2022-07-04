03e6cbfeb603846a8bb194cef19c5cfe2e753a44
relias08 <relias08@gmail.com>
2022-07-04 16:38:26 -0400

    added YY

--- hungry.py
+++ hungry.py
@@ -1,6 +1,6 @@
 hungry = input("are you hungry?")
 
-if hungry == "yes" and hungry == 'Y':
+if hungry == "yes" and hungry == 'YY':
     print('eat samosa')
     print('eat pizza')
     print('burger')
